ac9af423c4a5296f632bc343f5c765dcf6a158e7
Filipp Minayev <Filippmi@gmail.com>
2020-11-12 20:14:19 -0700

    Done, i had a lot of help with this one.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(num)
+    if num <=1
+        return false
+    end
+    range = (2..num - 1).to_a
+    range.each do |number|
+        if num % number == 0
+            return false
+        end
+    end   
+    return true 
+end
